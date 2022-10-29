9e3e7c04271b500bfc5871bdadff5d9df59d3934
Dimas Muhammad Rizki <dimas.muhammad.rizki.tik22@mhsw.pnj.ac.id>
2022-10-29 14:30:07 +0700

    Codingan C++

---
+++ Codingan Kuliah/22-09-2022/C.Menghitung_Luas_Segitiga/C.Menghitung_Luas_Segitiga.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+//code by Molestum314 {Dimas M Rizki}
+using namespace std;
+int main()
+{
+	int a, t;
+	
+	cout << "====================================" << endl;
+	cout << "===== MENGHITUNG LUAS SEGITIGA =====" << endl;
+	cout << "====================================" << endl;
+	cout << endl;
+	
+	//Input
+	cout << "Masukkan alas segitiga   : "; cin >> a;
+	cout << "Masukkan tinggi segitiga : "; cin >> t;
+	
+	cout << "-----------------------------------" << endl;
+	
+	//Output
+	cout << "Hasil Luas segitiga      : "<< 0.5 * a * t << endl;
+	
+	return 0;
+}
